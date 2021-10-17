c70599ecb31517eba8e8c17182279f9f5a5b316e
valmirnjr <junior.valmir6@gmail.com>
2021-10-17 21:49:47 +0200

    chore: creates common ctes and funcs

---
+++ src/Common/common.cpp
@@ -0,0 +1,26 @@
+#include "common.hpp"
+
+void HPS::print(const std::vector<std::string> &v) {
+  std::cout << "{ ";
+  for (int i = 0; i < v.size(); i++) {
+    std::cout << v[i];
+    if (i != v.size() - 1) {
+      std::cout << ", ";
+    }
+  }
+  std::cout << " }" << std::endl;
+}
+
+void HPS::print(const dict &d) {
+  std::cout << "{ ";
+  int i = 0;
+  for (auto const &p : d) {
+    std::cout << "\t" << p.first << ": " << p.second;
+    if (i != d.size() - 1) {
+      std::cout << ", ";
+    }
+    std::cout << std::endl;
+    i++;
+  }
+  std::cout << "}" << std::endl;
+}
---
+++ src/Common/common.hpp
@@ -0,0 +1,47 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_set>
+#include <iostream>
+
+namespace HPS {
+  namespace constants {
+    // File property keys
+    constexpr char TYPE[] = "TYPE";
+    constexpr char LABEL[] = "LABEL";
+    constexpr char COMPONENTS[] = "COMPONENTS";
+
+    // Component types
+    constexpr char BUS[] = "BUS";
+    constexpr char PLATFORM[] = "PLATFORM";
+    constexpr char PROGRAM[] = "PROGRAM";
+
+    const std::unordered_set<std::string> DEFAULT_KEYS = {
+      TYPE, LABEL, BUS, PLATFORM, COMPONENTS
+    };
+
+    // Instructions
+    constexpr char NOP[] = "NOP";
+    constexpr char ADD[] = "ADD";
+    constexpr char SUB[] = "SUB";
+    constexpr char MUL[] = "MUL";
+    constexpr char DIV[] = "DIV";
+
+    const std::unordered_set<std::string> INSTRUCTION_NAMES = {
+      NOP, ADD, SUB, MUL, DIV
+    };
+    
+    enum FileStyle {DICT, LINE};
+  }
+
+  typedef std::map<std::string, std::string> dict;
+
+  void print(const std::vector<std::string>&);
+  void print(const dict&);
+}
+
+
+#endif
