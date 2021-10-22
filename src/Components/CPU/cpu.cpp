c4475ec65ddcc2b0550c45f6bafa9363a00ab2e4
valmirnjr <junior.valmir6@gmail.com>
2021-10-23 00:56:29 +0200

    update: set CPU attrs and print it

--- src/Components/CPU/cpu.cpp
+++ src/Components/CPU/cpu.cpp
@@ -1,18 +1,61 @@
 #include "cpu.hpp"
 #include <iostream>
+#include <exception>
 
 using std::unique_ptr;
+using std::string;
 using HPS::CPU;
 using HPS::Component;
 
 CPU::CPU() {}
 
-CPU::CPU(dict &d) {
-  std::cout << "Creating CPU: " << d[constants::LABEL] << std::endl;
+CPU::CPU(const string &label, const int &numCores, const double &frequency)
+  : activeCore(0), numCores(numCores), frequency(frequency) {
+  this->label = label;
+  std::cout << *this;
 }
 
 void CPU::simulate() {}
 
-unique_ptr<Component> CPU::makeFromFileContent(dict d) {
-  return unique_ptr<Component>(new CPU(d));
+unique_ptr<Component> CPU::makeFromFileContent(dict &d) {
+  string label;
+  int numCores;
+  double frequency;
+
+  if (d.count(constants::LABEL) > 0) {
+    label = d[constants::LABEL];
+  }
+  if (d.count(constants::CORES) > 0) {
+    numCores = cstrToInt(d[constants::CORES].c_str());
+    if (numCores <= 0) {
+      throw std::invalid_argument("CPU Number of cores should be positive.");
+    }
+  }
+  if (d.count(constants::FREQUENCY) > 0) {
+    char *p;
+    frequency = std::stod(d[constants::FREQUENCY]);
+    if (frequency <= 0.0) {
+      throw std::invalid_argument("CPU Frequency should be positive.");
+    }
+  }
+  
+  return unique_ptr<Component>(new CPU(label, numCores, frequency));
 }
+
+void CPU::setProgram(Program p) {
+  prog = p;
+}
+
+std::ostream& CPU::outstream(std::ostream &out) {
+  out << constants::TYPE << ": {" << std::endl;
+  out << "\t" << constants::LABEL << ": " << label << std::endl;
+  out << "\t" << constants::CORES << ": " << numCores << std::endl;
+  out << "\t" << constants::FREQUENCY << ": " << frequency << std::endl;
+  // out << "\t" << constants::PROGRAM << ": " <<  << std::endl;
+  out << "}" << std::endl;
+  return out;
+}
+
+std::ostream & HPS::operator<<(std::ostream &os, CPU &b) {
+  return b.outstream(os);
+};
--- src/Components/CPU/cpu.hpp
+++ src/Components/CPU/cpu.hpp
@@ -1,16 +1,29 @@
 #ifndef CPU_HPP
 #define CPU_HPP
 
+#include <memory>
 #include "../component.hpp"
+#include "../../Program/program.hpp"
+#include "../../Register/register.hpp"
 
 namespace HPS {
   class CPU : public Component {
+    Program prog;
+    Register reg;
+    double frequency;
+    int numCores;
+    int activeCore;
+  
   public:
     CPU();
-    CPU(dict&);
+    CPU(const std::string&, const int&, const double&);
     void simulate();
-    std::unique_ptr<Component> makeFromFileContent(dict);
+    std::unique_ptr<Component> makeFromFileContent(dict&);
+    void setProgram(Program);
+    std::ostream& outstream(std::ostream &out);
   };
+
+  std::ostream & operator<<(std::ostream&, CPU&);
 }
 
 
