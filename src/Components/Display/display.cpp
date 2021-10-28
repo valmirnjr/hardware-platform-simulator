d0d1a624f47040caad06a55cf6d954a026a6b997
valmirnjr <junior.valmir6@gmail.com>
2021-10-29 01:46:27 +0200

    update: display make and simulate methods

--- src/Components/Display/display.cpp
+++ src/Components/Display/display.cpp
@@ -1,20 +1,47 @@
 #include "display.hpp"
-#include <iostream>
 
 using std::shared_ptr;
+using std::string;
+using std::vector;
 using HPS::Display;
 using HPS::Component;
 
 Display::Display() {}
 
-Display::Display(dict &d) {
-  std::cout << "Creating Display: " << d[constants::LABEL] << std::endl;
-}
+Display::Display(const double &refreshRate) : refreshRate(refreshRate) { }
 
-void Display::simulate() {}
+void Display::simulate() {
+  if (!source) return;
+  
+  static int waitingTime = 0;
+  string buffer = "";
+  if (waitingTime <= 0) {
+    DataValue data = source->read();
+    while (data.valid) {
+      buffer += std::to_string(data.value);
+      data = source->read();
+    }
+    waitingTime = refreshRate;
+  }
+  waitingTime--;
+  std::cout << buffer;
+}
 
 shared_ptr<Component> Display::makeFromFileContent(dict &d) {
-  return shared_ptr<Component>(new Display(d));
+  // Check if all necessary keys are present in the dictionary
+  vector<string> mandatoryKeys({ constants::REFRESH });
+  vector<string> missingKeys = getDictMissingKeys(d, mandatoryKeys);
+  if (missingKeys.size() > 0) {
+    string errorMsg = "Display dictionary is missing the following key(s): ";
+    for (auto const &k : missingKeys) {
+      errorMsg += k + " ";
+    }
+    throw std::invalid_argument(errorMsg);
+  }
+
+  std::cout << "Creating Display" << std::endl;
+
+  double refreshRate = std::stod(d[constants::REFRESH]);
+
+  return shared_ptr<Component>(new Display(refreshRate));
 }
-
-
--- src/Components/Display/display.hpp
+++ src/Components/Display/display.hpp
@@ -2,16 +2,21 @@
 #define DISPLAY_HPP
 
 #include <memory>
-#include "../component.hpp"
+#include "../Readable/readable.hpp"
+
+// Used by display.cpp
+#include <string>
+#include <vector>
+#include <iostream>
 
 namespace HPS {
   class Display : public Component {
-    std::shared_ptr<Component> source;
+    std::shared_ptr<ReadableComponent> source;
     double refreshRate;
   
   public:
     Display();
-    Display(dict&);
+    Display(const double&);
     void simulate();
     std::shared_ptr<Component> makeFromFileContent(dict&);
   };
